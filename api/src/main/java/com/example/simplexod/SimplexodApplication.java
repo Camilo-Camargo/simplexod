e7d8fad49317954f40af659822471a56fa689fca
Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>
2023-03-08 18:55:08 -0500

    Integrate Spring Boot and Reactjs

---
+++ api/src/main/java/com/example/simplexod/SimplexodApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplexod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplexodApplication.class, args);
+	} 
+	
+}
---
+++ api/src/test/java/com/example/simplexod/SimplexodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplexod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplexodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
